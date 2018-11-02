c940f955447bc7ea949c33908eabf961bcce3e45
Mallory Hancock <malloryhancock89@gmail.com>
2018-11-02 15:28:34 -0700

    first pass at plugins

---
+++ plugins/rollcall.py
@@ -0,0 +1,6 @@
+import pinnhook.plugin as plugin
+
+@plugin.register('!rollcall', help_text='Gives a short message about what this bot does')
+def rollcall(msg):
+    return plugin.message("I act as a helper for the town's admins! For more info see https://github.com/tildetown/adminbot .")
+
