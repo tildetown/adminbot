5df22fc64150794d9ce1bcbc8961326a6c6c12c9
Mallory Hancock <malloryhancock89@gmail.com>
2018-11-30 18:13:38 +0000

    update admins for psa

--- plugins/psa.py
+++ plugins/psa.py
@@ -1,9 +1,13 @@
 import pinhook.plugin as p
 
 ADMINS = [
-    'vilmibm',
-    'archangelic'
+    'vilmibim',
+    'archangelic',
+    'l0010o0001l',
+    'karlen',
+    'equa'
 ]
+                                                    ]
 
 @p.register('!psa', help_text='Usable only by the town admins, this causes adminbot to anonymously relay a message to #tildetown.')
 def psa(msg):
