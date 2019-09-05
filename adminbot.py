4c05de17427f0f8714ccfe3a8c309753eeb6b8c0
Mallory Hancock <malloryhancock89@gmail.com>
2019-09-05 11:13:25 -0700

    add webhook support with basic auth

--- adminbot.py
+++ adminbot.py
@@ -1,8 +1,17 @@
 import json
 import re
+import threading
 import os
 
+from flask import Flask, request
+from flask_httpauth import HTTPBasicAuth
 import pinhook.bot
+from werkzeug.security import check_password_hash
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+with open('users.json') as u:
+    USERS = json.load(u)
 
 class AdminBot(pinhook.bot.Bot):
     townie_re = re.compile('townie-[a-f0-9]{16}')
@@ -47,17 +56,33 @@ class AdminBot(pinhook.bot.Bot):
         # this is going to be handled by a listener plugin
         pass
 
+@auth.verify_password
+def verify_password(username, password):
+    if username in USERS:
+        return check_password_hash(USERS.get(username), password)
+    return False
+
+with open('pw.secret') as p:
+    pw = p.read().strip()
+with open('channels.json') as c:
+    channels = json.load(c)
+ops = [
+    'vilmibm',
+    'archangelic',
+    'equa',
+    'l0010o0001l'
+]
+bot = AdminBot(channels, 'adminbot', 'localhost', ops=ops, ns_pass=pw)
+
+@app.route('/notify', methods=['POST'])
+@auth.login_required
+def notify_admins():
+    channel = request.json['channel']
+    message = request.json['message']
+    bot.connection.privmsg(channel, message)
 
 if __name__ == '__main__':
-    with open('pw.secret') as p:
-        pw = p.read().strip()
-    with open('channels.json') as c:
-        channels = json.load(c)
-    ops = [
-        'vilmibm',
-        'archangelic',
-        'equa',
-        'l0010o0001l'
-    ]
-    bot = AdminBot(channels, 'adminbot', 'localhost', ops=ops, ns_pass=pw)
-    bot.start()
+    bot_thread = threading.Thread(target=bot.start)
+    bot_thread.start()
+    flask_thread = threading.Thread(target=app.run)
+    flask_thread.start()
---
+++ create_user.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import getpass
+import json
+
+from werkzeug.security import generate_password_hash
+
+with open('users.json') as u:
+    users = json.load(u)
+
+user = input(prompt='username> ')
+password = getpass.getpass(prompt='password> ')
+
+users[user] = generate_password_hash(password)
+
+with open('users.json', 'w') as u:
+    json.dump(users, u)
