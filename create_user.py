09e1589d54f0cb02e6e29dcb9e2f93c6867513b1
Mallory Hancock <malloryhancock89@gmail.com>
2019-09-06 09:26:20 -0700

    create users file, set file perms

--- create_user.py
+++ create_user.py
@@ -1,12 +1,18 @@
 #! /usr/bin/env python3
-
 import getpass
 import json
+import os
 
 from werkzeug.security import generate_password_hash
 
-with open('users.json') as u:
-    users = json.load(u)
+if 'users.json' in os.listdir():
+    with open('users.json') as u:
+        users = json.load(u)
+else:
+    with open('users.json', 'w') as u:
+        json.dump({}, u)
+    os.chmod('users.json', 0o600)
+    users = {}
 
 user = input('username> ')
 password = getpass.getpass(prompt='password> ')
